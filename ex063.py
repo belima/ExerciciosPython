a1e9d00cce2ac791b39049171701f5b549c99d3b
Bê Lima <lima.bernardo@gmail.com>
2020-05-09 17:57:40 -0300

    ex063 iniciado

--- ex063.py
+++ ex063.py
@@ -2,3 +2,23 @@
 Escreva um programa q leia um número n, inteiro, e mostre na tela os n primeiros elementos de uma Fibonacci.
 '''
 
+n = termos = 1
+a1 = 0
+a2 = 1
+a3 = a1 + a2
+while termos != 0:
+    termos = int(input('\nQuantos termos da Fibonacci vc quer ver?\n>>> '))
+    if termos != 0:
+        print('{}, {}, '.format(a1, a2),end='')
+        while n <= termos:
+            print('{}, '.format(a3),end='')
+            a1 = a2
+            a2 = a3
+            a3 = a1 + a2
+            n += 1
+        n = 1
+        a1 = 0
+        a2 = 1
+        a3 = a1 + a2
+print('Nóis')
+
