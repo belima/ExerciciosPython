5930ad8d0ae00f4a74510afb0cb3f7130cdb0252
Bê Lima <lima.bernardo@gmail.com>
2020-05-10 05:14:51 -0300

    Melhorias na exibição de resultados no ex060.py

--- ex060.py
+++ ex060.py
@@ -1,17 +1,22 @@
 '''
 leia um número e mostre seu fatorial.
 '''
+# todo Criar versão com for
+
 n = 1
 
 while n != 0:
         n = int(input('Digite um número para calcular seu fatorial (0 para sair) >'))
-        cont = n - 1
-        fat = n
+        cont = n
+        fat = 1
 
         if n != 0:
+            print('{}! = '.format(n), end='')
             while cont > 0:
                 fat *= cont
+                print('{}'.format(cont),end='')
+                print(' * ' if cont > 1 else ' = ', end='')
                 cont -= 1
-            print('{}! = {}'.format(n, fat))
+            print('{}'.format(fat))
         else:
             print('Nóis')
