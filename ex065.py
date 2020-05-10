26d8d98deeb8c5cf239b86c1cf080797338674f5
Bê Lima <lima.bernardo@gmail.com>
2020-05-10 20:33:31 -0300

    pequenas alterações no ex065.py

--- ex065.py
+++ ex065.py
@@ -4,18 +4,16 @@ mostre a média entre todos os valores e quais foram o maior e menor valores lid
 '''
 
 n = int(input('Insira um valor: '))
-menor = n
-maior = n
+menor = maior = total = n
 i = 1
-total = n
 
-cont = str(input('Deseja inserir mais valores?(S/N) '))
+cont = str(input('Deseja inserir mais valores?(S/N) ')).strip().upper()
 
-if cont not in 'nN':
-    while cont not in 'nN':
-        if cont not in 'nN':
+if cont == 'S':
+    while cont == 'S':
+        if cont == 'S':
             n = int(input('Insira um valor: '))
-            cont = str(input('Deseja inserir mais valores?(S/N) '))
+            cont = str(input('Deseja inserir mais valores?(S/N) ')).strip().upper()
             total += n
             i += 1
             if n > maior:
@@ -24,6 +22,9 @@ if cont not in 'nN':
                 menor = n
 if i > 1:
     print('A média dos {} valores digitados é {:.2f}.'.format(i, total / i))
-    print('O menor valor digitado foi {} e o maior {}.'.format(menor, maior))
+    if menor != maior:
+        print('O menor valor digitado foi {} e o maior foi {}.'.format(menor, maior))
+    else:
+        print('Os valores digitados foram iguais.')
 else:
     print('O único valor digitado foi {}'.format(n))
