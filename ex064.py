d221692739138667cb2106d5895f1ee233ac654a
Bê Lima <lima.bernardo@gmail.com>
2020-05-10 20:09:30 -0300

    pequenas melhorias no ex064.py

--- ex064.py
+++ ex064.py
@@ -2,18 +2,15 @@
 leia vários números inteiros, até q o usuário digite 999.
 Mostre quantos números foram digitados e a soma entre eles, desconsiderando o flag (999)
 '''
-
+qtd = soma = 0
 n = int(input('Digite um número [999 p/ sair]\n> '))
-qtd = 1
-soma = n
-while n != 999:
-    n = int(input('Mais um [999 p/ sair]\n> '))
-    if n != 999:
-        qtd += 1
-        soma += n
 
+while n != 999:
+    qtd += 1
+    soma += n
+    n = int(input('Mais um [999 p/ sair]\n> '))
 if qtd == 1:
     print('Você digitou 1 número, q foi {}.'.format(soma))
 else:
-    print('Você digitou {} números e a soma entre todos eles é {}.'.format(qtd, soma))
+    print('Você digitou {} números e a soma entre eles é {}.'.format(qtd, soma))
 print('Nóis')
