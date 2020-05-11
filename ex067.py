b518c3e60dd7ced4a1c2c478bcb0a486ac798fd6
Bê Lima <lima.bernardo@gmail.com>
2020-05-11 12:26:26 -0300

    ex067.py resolvido

--- ex067.py
+++ ex067.py
@@ -2,3 +2,21 @@
 mostre a tabuada de vários números, 1 de cada vez, para cada valor digitado.
 o programa para qd o número solicitado for negativo.
 '''
+from time import sleep
+
+print('~' * 20)
+print('TABUADA!')
+print('~' * 20)
+
+while True:
+    num = int(input('Qual tabuada vc quer ver agora?\n[valor negativo p/ fechar]\n>>> '))
+    if num < 0:
+        break
+    print('~' * 20)
+    for i in range(0,11):
+        print(f'{num} * {i:<2} = {num * i}')
+    print('~' * 20)
+print('~' * 20)
+print('Encerrando TABUADA!...')
+sleep(1)
+print('Nóis')
