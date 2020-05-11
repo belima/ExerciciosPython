a9ef07e08d0d4e9122ab81fd6ccff18c7c59c4f7
Bê Lima <lima.bernardo@gmail.com>
2020-05-11 12:11:26 -0300

    ex066.py resolvido

--- ex066.py
+++ ex066.py
@@ -2,3 +2,13 @@
 leia varios numeros inteiros. pare qd o usuário digitar 999.
 mostre quantos numeros foram digitados e ql a soma entre eles, desconsiderando o flag
 '''
+
+cont = soma = 0
+
+while True:
+    num = int(input('Digite um número: '))
+    if num == 999:
+        break
+    cont += 1
+    soma += num
+print(f'A soma entre os {cont} números digitados é {soma}.')
