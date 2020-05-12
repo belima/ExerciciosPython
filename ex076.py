be4579530c339ca9e04678a07fa41879d302f8d1
Bê Lima <lima.bernardo@gmail.com>
2020-05-12 13:38:54 -0300

    exercícios 076 e 077 resolvidos

--- ex076.py
+++ ex076.py
@@ -3,3 +3,15 @@ crie uma tupla unica com nomes de produtos e seus respectivos preços, na sequê
 mostre uma listagem de preços, organizando os dados em forma tabular
 '''
 
+prodpreços = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90, 'Estojo', 25, 'Transferidor', 4.2, 'Compasso', 9.99,
+              'Mochila', 120.32, 'Canetas', 22.3, 'Livro', 34.9)
+
+linha = '-' * 50
+
+print(linha)
+print(f'{"LISTAGEM DE PREÇOS":^50}')
+print(linha)
+
+for n in range(0, len(prodpreços), 2):
+    print(f'{prodpreços[n]:.<40}R${prodpreços[n+1]:>7.2f}')
+print(linha)
--- ex077.py
+++ ex077.py
@@ -2,3 +2,13 @@
 crie uma tupla com várias palavras(não use acentos).
 mostre, para cada palavra, quais são suas vogais.
 '''
+
+palavras = ('caneca', 'monitor', 'joypad', 'simulador', 'mouse', 'alcool', 'pandemia', 'coronavirus')
+vogais = ('a', 'e', 'i', 'o', 'u')
+
+for p in range(0, len(palavras)):
+    print(f'As vogais em "{palavras[p]}" são ', end='')
+    for l in range(0, len(palavras[p])):
+        if palavras[p][l] in vogais:
+            print(f'{palavras[p][l]} ', end='')
+    print()
