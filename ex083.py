bffc274bbb3b12006892bdfedc842ab87533ba95
Bê Lima <lima.bernardo@gmail.com>
2020-05-13 13:08:48 -0300

    ex083.py resolvido

--- ex083.py
+++ ex083.py
@@ -2,3 +2,22 @@
 leia uma expressão qualquer que use (parenteses).
 analise se a expressão está com os parenteses abertos e fechados na ordem correta.
 """
+parenteses = 0
+ordemcerta = True
+
+expr = input('Digite uma expressão matemática: ')
+
+for i in expr:
+    if i == '(':
+        parenteses += 1
+    elif i == ')':
+        parenteses -= 1
+    if parenteses < 0:
+        print('Expressão inválida!')
+        ordemcerta = False
+        break
+if ordemcerta == True:
+    if parenteses == 0:
+        print('Expressão válida!')
+    else:
+        print('Expressão inválida!')
