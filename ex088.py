10193de15555f8aa30f1068335b87bd9d9636d00
Bê Lima <lima.bernardo@gmail.com>
2020-05-13 19:30:50 -0300

    ex088.py resolvido

--- ex088.py
+++ ex088.py
@@ -3,3 +3,25 @@ ajude um jogador da mega sena a criar palpites.
 pergunte quantos jogos serão criados e sorteie 6 numeros,sem repetição, entre 1 e 60 para cada jogo.
 cadastrando tudo em uma lista composta.
 '''
+from random import randint
+from time import sleep
+
+jogo = []
+
+print('~~' * 20)
+print(f'{"PALPITEIRO":^40}')
+print('~~' * 20)
+
+no = int(input('Quantos jogos você quer? > '))
+
+print(f'~~~~~~~~~~ SORTEANDO {no} JOGOS ~~~~~~~~~~~\n')
+for j in range(0, no):
+    while len(jogo) < 6:
+        temp = randint(1, 60)
+        if temp not in jogo:
+            jogo.append(temp)
+    sleep(0.6)
+    print(f'Jogo {j + 1}: {sorted(jogo)}')
+    jogo.clear()
+sleep(0.6)
+print('\n~~~~~~~~~~~~~~ BOA SORTE! ~~~~~~~~~~~~~~')
