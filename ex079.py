0a45b2698e3a3a929cf3b7b9a426d7b18f8382cc
Bê Lima <lima.bernardo@gmail.com>
2020-05-13 11:01:24 -0300

    ex079.py resolvido

--- ex079.py
+++ ex079.py
@@ -2,3 +2,25 @@
 leia diversos valores e armazene em uma lista, não adicionando valores repetidos.
 exiba todos os valores únicos digitados, em ordem crescente.
 """
+
+lista = []
+continua = ' '
+
+while True:
+    valor = int(input('Digite um valor: '))
+    if valor not in lista:
+        lista.append(valor)
+        print('Valor adicionado...')
+    else:
+        print('Valor já existente. Não adicionado novamente...')
+
+    while continua not in 'SN':
+        continua = input('Deseja continuar? [S/N] ').strip().upper()[0]
+    if continua == 'N':
+        continua = ' '
+        break
+    else:
+        continua = ' '
+
+print('~~' * 20)
+print(f'Os valores adicionados, em ordem crescente, são: {sorted(lista)}')
