c2f2251fc876edc18a705a37169c9f16da415e45
Bê Lima <lima.bernardo@gmail.com>
2020-05-13 18:50:05 -0300

    ex086.py resolvido

--- ex086.py
+++ ex086.py
@@ -2,3 +2,16 @@
 crie uma matriz 3x3 com valores lidos pelo teclado
 mostre a matriz formatada corretamente
 '''
+
+matriz = []
+linha = []
+
+for c in range(0, 3):
+    for i in range(0,3):
+        linha.append(int(input(f'Digite o valor da posição [{c}, {i}] > ')))
+    matriz.append(linha[:])
+    linha.clear()
+
+print('~~' * 30)
+for l in range(0,3):
+    print(f'[ {matriz[l][0]} ][ {matriz[l][1]} ][ {matriz[l][2]} ]')
