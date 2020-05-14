7b6ab7d0ef11f9820efdf2bf73971f36d5defe9b
Bê Lima <lima.bernardo@gmail.com>
2020-05-14 19:52:36 -0300

    ex 091 e 092 resolvidos

--- ex091.py
+++ ex091.py
@@ -3,3 +3,26 @@ crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios
 Guarde esses resultados em um dicionário.
 Coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 '''
+from time import sleep
+from random import randint
+from operator import itemgetter
+
+jogos = {}
+cont = 1
+
+for i in range(1,5):
+    jogos[f'jogador{i}'] = randint(1,6)
+
+print('Valores sorteados:')
+for k, v in jogos.items():
+    sleep(1)
+    print(f"{k} tirou {v} no dado.")
+
+jogosord = dict(sorted(jogos.items(), key=itemgetter(1),reverse=True))
+
+print("-=" * 16)
+print("  == RANKING DOS JOGADORES ==  ")
+for k, v in jogosord.items():
+    sleep(1)
+    print(f"{cont}º lugar: {k} com {v}.")
+    cont += 1
--- ex092.py
+++ ex092.py
@@ -1,5 +1,23 @@
 '''
-leia nome, ano de nasimento e carteira de trabalho e cadastre-os (com idade) em um dicionário.
-Se a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o saçário.
+leia nome, ano de nascimento e carteira de trabalho e cadastre-os (com idade) em um dicionário.
+Se a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
 Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 '''
+
+from datetime import date
+
+trabalhador = {}
+
+trabalhador['nome'] = str(input('Nome > ')).strip().capitalize()
+nasc = int(input('Ano de nascimento > '))
+trabalhador['idade'] = date.today().year - nasc
+trabalhador['ctps'] = int(input('Carteira de Trabalho [0 não tem] > '))
+
+if trabalhador['ctps'] != 0:
+    trabalhador['contratação'] = int(input('Ano de contratação > '))
+    trabalhador['salário'] = float(input('Salário > '))
+    trabalhador['aposentadoria'] = (trabalhador['contratação'] + 35) - nasc
+
+print('~~' * 20)
+for k, v in trabalhador.items():
+    print(f"  - {k} tem o valor {v}")
