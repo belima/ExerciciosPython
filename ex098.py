be4ce230a546ca61efdbae22ab1633d7c6f08105
Bê Lima <lima.bernardo@gmail.com>
2020-05-16 19:40:07 -0300

    ex098.py resolvido

--- ex098.py
+++ ex098.py
@@ -1,8 +1,45 @@
 '''
-faça um programa que tenha uma função chamada contador(), que receba trÊs paramêtros: 'inicio', 'fim', e 'passo' e
+faça um programa que tenha uma função chamada contador(), que receba três parâmetros: 'inicio', 'fim', e 'passo' e
 exiba a contagem na tela.
 Seu programa deve realizar três contagens através da função criada:
     a) de 1 a 10, de 1 em 1
     b) de 10 a 0, de 2 em 2
     c) uma contagem personalizada.
 '''
+from time import sleep
+
+def separa(n):
+    print("~" * n)
+
+def contador(inicio, fim, passo):
+    if passo == 0:
+        passo = 1
+
+    separa(40)
+    print(f"Contagem de {inicio} até {fim} de {passo} em {passo}:")
+
+    if inicio < fim:
+        fim += 1
+        passo = abs(passo)
+    elif inicio > fim:
+        fim -= 1
+        passo = (abs(passo) * -1)
+
+    for c in range(inicio, fim, passo):
+        print(f"{c} ", end='')
+        sleep(0.2)
+    print("FIM!")
+
+
+contador(1, 10, 1)
+
+contador(10, 0, 2)
+
+separa(40)
+
+print("Agora é sua vez de personalizar a contagem!")
+i = int(input("Início > "))
+f = int(input("Fim > "))
+p = int(input("Passo > "))
+
+contador(i, f, p)
