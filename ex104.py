e9800b10b013c4869bef37189b937769580b2c85
Bê Lima <lima.bernardo@gmail.com>
2020-05-17 19:43:42 -0300

    ex104.py resolvido

--- ex104.py
+++ ex104.py
@@ -2,3 +2,19 @@
 crie um programa que tenha a função leiaint(), que vai funcionar como um input(), porém aceitando apenas valores
 numéricos inteiros.
 '''
+def leiaint(perg=None):
+    while True:
+        print(perg, end='')
+        inteiro = input()
+        if inteiro.isnumeric():
+            break
+        print('Apenas números inteiros são aceitos.')
+
+    return int(inteiro)
+
+
+a = leiaint('Valor A: ')
+b = leiaint('Valor B: ')
+
+s = a + b
+print(s)
